44961d539b889290258c0263a2bf0bc4e8f5ce63
ritu-bacancytechnology <71626746+ritu-bacancytechnology@users.noreply.github.com>
2020-12-25 12:53:03 +0530

    Added Land Success Filter

--- script.js
+++ script.js
@@ -34,6 +34,25 @@ $(document).ready(function(){
       console.log("Error");
     });
    })
+
+   $(".successLanding").on("click",function(){
+    var apiUrl = 'https://api.spacexdata.com/v3/launches?limit=100&launch_success=true';
+    fetch(apiUrl).then(response => {
+      return response.json();
+    }).then(data => {
+      // Work with JSON data here
+      var ans = data;
+     var val = this.id;
+     console.log(val);
+     console.log('Inside Landing');
+     
+     filterSuccessLanding(data, val);
+    
+    }).catch(err => {
+      // Do something for an error here
+      console.log("Error");
+    });
+   })
 });
 
 function filterYear(data,year){
@@ -60,9 +79,9 @@ function filterYear(data,year){
         var launch_year = ly[i].launch_year;
         var mission_ids = ly[i].mission_id;
         var launch_success = ly[i].launch_success;   
+        var land_success = ly[i].rocket.first_stage.cores[0].land_success;
 
-
-        var card = ' <div class="col-md-3 card-style card"><div><img src='+ img +' alt="image" style="height: 100px;width: 100px;" /></div><div><h4 class="h4">'+ name +' #'+ number +'</h4> <p>Mission Ids : </p><ul id="mission"></ul><p>Launch Year : '+ launch_year +'</p><p>Successful Launch :'+ launch_success +'</p><p>Successful Landing : Launch Landing</p></div></div>';
+        var card = ' <div class="col-md-3 card-style card"><div><img src='+ img +' alt="image" style="height: 100px;width: 100px;" /></div><div><h4 class="h4">'+ name +' #'+ number +'</h4> <p>Mission Ids : </p><ul id="mission"></ul><p>Launch Year : '+ launch_year +'</p><p>Successful Launch :'+ launch_success +'</p><p>Successful Landing :'+ land_success +'</p></div></div>';
 
         $("#mission").append(appendMissionId(mission_ids));
         $("#ans").append(card);
@@ -100,9 +119,50 @@ function filterSuccessLaunch(data, val){
         var launch_year = ly[i].launch_year;
         var mission_ids = ly[i].mission_id;
         var launch_success = ly[i].launch_success;   
+        var land_success = ly[i].rocket.first_stage.cores[0].land_success;
+
+        var card = ' <div class="col-md-3 card-style card"><div class="img-div"><img src='+ img +' alt="image" style="height: 100px;width: 100px;" /></div><div><h4 class="h4">'+ name +' #'+ number +'</h4> <p>Mission Ids : </p><ul id="mission"></ul><p>Launch Year : '+ launch_year +'</p><p>Successful Launch :'+ launch_success +'</p><p>Successful Landing : '+ land_success +'</p></div></div>';
+
+        $("#mission").append(appendMissionId(mission_ids));
+        $("#ans").append(card);
+    }  
+}
 
 
-        var card = ' <div class="col-md-3 card-style card"><div class="img-div"><img src='+ img +' alt="image" style="height: 100px;width: 100px;" /></div><div><h4 class="h4">'+ name +' #'+ number +'</h4> <p>Mission Ids : </p><ul id="mission"></ul><p>Launch Year : '+ launch_year +'</p><p>Successful Launch :'+ launch_success +'</p><p>Successful Landing : Launch Landing</p></div></div>';
+function filterSuccessLanding(data, val){
+
+    var ly = [];
+    var k = 0;
+    var v = "";
+    if(val == "successTrue")
+    {
+        v = true;
+    }
+    else
+    {
+        v = false;
+    }
+    for(let i=0;i<data.length;i++)
+    {
+        // console.log(data[i].rocket.first_stage.cores[0].land_success);
+        if(data[i].rocket.first_stage.cores[0].land_success == v)
+        {
+            ly[k++] = data[i];
+        }
+    }
+    console.log(ly);
+    $("#ans").html("");
+    for(let i=0;i<k;i++)
+    {
+        var img = ly[i].links.mission_patch;
+        var name = ly[i].mission_name;
+        var number = ly[i].flight_number;
+        var launch_year = ly[i].launch_year;
+        var mission_ids = ly[i].mission_id;
+        var launch_success = ly[i].launch_success;   
+        var land_success = ly[i].rocket.first_stage.cores[0].land_success;
+
+        var card = ' <div class="col-md-3 card-style card"><div class="img-div"><img src='+ img +' alt="image" style="height: 100px;width: 100px;" /></div><div><h4 class="h4">'+ name +' #'+ number +'</h4> <p>Mission Ids : </p><ul id="mission"></ul><p>Launch Year : '+ launch_year +'</p><p>Successful Launch :'+ launch_success +'</p><p>Successful Landing :'+ land_success +'</p></div></div>';
 
         $("#mission").append(appendMissionId(mission_ids));
         $("#ans").append(card);
